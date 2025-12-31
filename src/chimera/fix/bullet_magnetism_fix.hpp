71655f6a5777524ffa7368c668f32aa3fcafad0d
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2025-12-31 05:36:34 -0700

    bullet_magnetism_fix.hpp

--- src/chimera/fix/bullet_magnetism_fix.hpp
+++ src/chimera/fix/bullet_magnetism_fix.hpp
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 #pragma once
 
-#include "../chimera_command.hpp"
+#include "../command/command.hpp" 
+#include "../chimera.hpp"
 
 // Inicializa el fix de bullet magnetism
 void bullet_magnetism_fix() noexcept;
 
 // Comando de consola para activar/desactivar
 ChimeraCommandError bullet_magnetism_command(size_t argc, const char **argv) noexcept;
+
