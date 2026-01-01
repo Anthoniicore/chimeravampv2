a6a9c408f4d7e058438303cec2cc274497e2347a
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-01-01 01:34:54 -0700

    bullet_magnetism_fix.cpp

--- src/chimera/fix/bullet_magnetism_fix.cpp
+++ src/chimera/fix/bullet_magnetism_fix.cpp
@@ -2,6 +2,7 @@
 
 #include "bullet_magnetism_fix.hpp"
 #include "../signature/hook.hpp"
+#include "../signature/signature.hpp"
 #include "../halo_data/table.hpp"
 #include "../halo_data/object.hpp"
 #include "../command/command.hpp"
@@ -58,36 +59,31 @@ namespace Chimera {
     }
 
     void bullet_magnetism_fix() noexcept {
-    static Hook bullet_hook;
-    auto *bullet_addr = get_chimera().get_signature("bullet_magnetism_sig").data();
-    write_function_override(
-        bullet_addr,
-        bullet_hook,
-        apply_bullet_magnetism,
-        nullptr
-    );
-    bullet_magnetism_enabled = true;
-}
-
-bool bullet_magnetism_command(int argc, const char **argv) noexcept {
-    if(argc == 1) {
-        bool new_value = STR_TO_BOOL(argv[0]);
-        if(new_value != bullet_magnetism_enabled) {
-            if(new_value) {
-                bullet_magnetism_fix();
-            } else {
-                static Hook bullet_hook;
-                bullet_hook.rollback();
-            }
-            bullet_magnetism_enabled = new_value;
-        }
+        static Hook bullet_hook;
+        auto *bullet_addr = get_signature("bullet_magnetism_sig").data();
+        write_function_override(
+            bullet_addr,
+            bullet_hook,
+            apply_bullet_magnetism,
+            nullptr
+        );
+        bullet_magnetism_enabled = true;
     }
-    console_output(BOOL_TO_STR(bullet_magnetism_enabled));
-    return true;
-}
 
-
-
-
-
-
+    bool bullet_magnetism_command(int argc, const char **argv) noexcept {
+        if(argc == 1) {
+            bool new_value = STR_TO_BOOL(argv[0]);
+            if(new_value != bullet_magnetism_enabled) {
+                if(new_value) {
+                    bullet_magnetism_fix();
+                } else {
+                    static Hook bullet_hook;
+                    bullet_hook.rollback();
+                }
+                bullet_magnetism_enabled = new_value;
+            }
+        }
+        console_output(BOOL_TO_STR(bullet_magnetism_enabled));
+        return true;
+    }
+} // namespace Chimera
