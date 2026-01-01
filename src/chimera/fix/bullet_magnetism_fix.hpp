78f343e78ecbcc6b02107fc5f5bae1b295635e6c
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-01-01 00:55:05 -0700

    bullet_magnetism_fix.hpp

--- src/chimera/fix/bullet_magnetism_fix.hpp
+++ src/chimera/fix/bullet_magnetism_fix.hpp
@@ -1,12 +1,9 @@
-// bullet_magnetism_fix.hpp
+// SPDX-License-Identifier: GPL-3.0-only
 #pragma once
 
-#include "../command/command.hpp"
-#include "../chimera.hpp"
+#include "signature.hpp"
 
 namespace Chimera {
-    bool bullet_magnetism_command(int argc, const char **argv) noexcept;
-    void bullet_magnetism_fix() noexcept;
-
+    // Declaración de la firma para el fix de bullet magnetism
+    extern Signature bullet_magnetism_sig;
 }
-
